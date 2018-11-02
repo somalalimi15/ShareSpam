00291e6ab7455d26a8be8a50a48c825971763c35
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-11-02 21:52:50 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);  
+});
+
+//spam1
+
+client.on('ready', async() => {
+var server = "502578149423448074"; // ايدي السررفر
+var channel = "507984864096550922";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**- SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM **')
+    },305);
+})
+
+client.login(process.env.BOT_TOKEN);
+///
+const Discord = require('discord.js');
+const client2 = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);  
+});
+
+//spam2
+
+client.on('ready', async() => {
+var server = "502578149423448074"; // ايدي السررفر
+var channel = "507984888641880071";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**- SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM **')
+    },305);
+})
+
+client.login(process.env.BOT_TOKEN2);
+
+
