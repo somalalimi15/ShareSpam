5011a7b2020e071f4addb84b81a9be64e276a43e
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-11-10 09:48:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,7 +24,38 @@ const client22 = new Discord.Client();
 const client23 = new Discord.Client();
 const client24 = new Discord.Client();
 const client25 = new Discord.Client();
+const client111 = new Discord.Client();
 
+console.log("Welcome Again !");
+ 
+ client111.on("guildMemberRemove", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+[ Welcome To Paranoia. ] ,
+سـيرفر سوآف ونااار  تعالوا كلكمممم , تشرفونا
+
+[ https://discord.gg/nAu2zcT ] ,
+[ https://images-ext-1.discordapp.net/external/5j2inxAmKsGZRr8oGuoeKCynVBzqItDw-eSyBA4mHpM/https/media3.giphy.com/media/3o6Zt6zRQw8yStXfxe/giphy.gif?width=432&height=324 ]
+الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+**`)
+}).catch(console.error)
+
+})
+ 
+client111.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+[ Welcome To Paranoia. ] ,
+سـيرفر سوآف ونااار  تعالوا كلكمممم , تشرفونا
+ 
+[ https://discord.gg/nAu2zcT ] ,
+[ https://images-ext-1.discordapp.net/external/5j2inxAmKsGZRr8oGuoeKCynVBzqItDw-eSyBA4mHpM/https/media3.giphy.com/media/3o6Zt6zRQw8yStXfxe/giphy.gif?width=432&height=324 ]
+الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+**`) 
+}).catch(console.error)
+})
 
 
   
@@ -259,6 +290,7 @@ client22.login(process.env.BOT_TOKEN22);
 client23.login(process.env.BOT_TOKEN23);
 client24.login(process.env.BOT_TOKEN24);
 client25.login(process.env.BOT_TOKEN25);
+client111.login(process.env.BOT_TOKEN111);
 
 
 
