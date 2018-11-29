fd5949b5344b874eb319a4ede7f086d6d22f66df
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-11-29 21:55:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,14 +31,11 @@ console.log("Welcome Again !");
  client111.on("guildMemberRemove", member => {
   member.createDM().then(function (channel) {
   return channel.send(` 
-**
-[ Welcome To Paranoia. ] ,
-سـيرفر سوآف ونااار  تعالوا كلكمممم , تشرفونا
-
-[ https://discord.gg/nAu2zcT ] ,
-[ https://images-ext-1.discordapp.net/external/5j2inxAmKsGZRr8oGuoeKCynVBzqItDw-eSyBA4mHpM/https/media3.giphy.com/media/3o6Zt6zRQw8yStXfxe/giphy.gif?width=432&height=324 ]
-الدعووة خاصة لك ي قلبي ... [ ${member}  ]
-**`)
+** Welcome To, Derba .. 
+ --------------------
+,- عـآدي نـخطفك ي لبه ؟ 
+, تعال ي عُمـري , انت واخوياك واستانسو , مسوين قيف اوايات ناآر و نظام انفايت حريقةة بعد الحقوو , :hearts: :fish_cake: 
+ [ https://discord.gg/EfGUAQ8 ] [ ${member}  ]. **`)
 }).catch(console.error)
 
 })
@@ -46,14 +43,11 @@ console.log("Welcome Again !");
 client111.on("guildMemberAdd", member => {
   member.createDM().then(function (channel) {
   return channel.send(` 
-**
-[ Welcome To Paranoia. ] ,
-سـيرفر سوآف ونااار  تعالوا كلكمممم , تشرفونا
- 
-[ https://discord.gg/nAu2zcT ] ,
-[ https://images-ext-1.discordapp.net/external/5j2inxAmKsGZRr8oGuoeKCynVBzqItDw-eSyBA4mHpM/https/media3.giphy.com/media/3o6Zt6zRQw8yStXfxe/giphy.gif?width=432&height=324 ]
-الدعووة خاصة لك ي قلبي ... [ ${member}  ]
-**`) 
+** Welcome To, Derba .. 
+ --------------------
+,- عـآدي نـخطفك ي لبه ؟ 
+, تعال ي عُمـري , انت واخوياك واستانسو , مسوين قيف اوايات ناآر و نظام انفايت حريقةة بعد الحقوو , :hearts: :fish_cake: 
+ [ https://discord.gg/EfGUAQ8 ] [ ${member}  ]. **`) 
 }).catch(console.error)
 })
 
