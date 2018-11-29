338acf9fecca00ccecc9221e9b0cd607239ade28
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-11-29 22:02:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const client2 = new Discord.Client();
+const client2 = new Discord.Client(); 
 const client3 = new Discord.Client();
 const client4 = new Discord.Client();
 const client5 = new Discord.Client();
